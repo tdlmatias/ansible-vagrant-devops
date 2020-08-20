6b2fec1729334d9fba2a17eded631b33f988792d
tmatias <tchize.matias@wetek.com>
2020-08-20 14:06:37 +0000

    Configure bracking

--- main.tf
+++ main.tf
@@ -1,14 +1,24 @@
 provider "aws" {
-    region = "${var.aws_region}"
-    shared_credentials_file = "${var.shared_cred_file}"
+    region = "eu-west-1"
     profile = "wetekdev"
 }
 
 resource "aws_instance" "wbc-preprod" {
     ami            = "ami-01b282b0f06ba5fd2"
     instance_type  = "t2.large"
+    key_name       = "wetek-devops-wbc"
+    associated_public_ip_address = true
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = "120"
+        delete_on_termination = true
+    }
 
     tags = {
-        Name = "wbc-preprod"
+        Name = "WBC-preprod"
     }
+}
+
+output "IPAddress" {
+    value = "${aws_instance.wbc-preprod.public_ip}"
 }
--- variable.tf
+++ variable.tf
@@ -8,3 +8,15 @@ variable "aws_region"  {
     description = "Region to launch Server's"
     default = "eu-west-1"
 }
+
+//module "key_pair" {
+//  source = "terraform-aws-modules/key-pair/aws"
+
+//  key_name   = "wetek-devops-wbc"
+//  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+
+//}
+
+data "tls_public_key" "wbc-preprod" {
+  private_key_pem = "${file("~/.ssh/wetek-devops-wbc.pem")}"
+}
