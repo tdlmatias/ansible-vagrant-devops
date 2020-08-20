345796833a4c64e0b9f12efcc1d7c1ec3c7817c4
tmatias <tchize.matias@wetek.com>
2020-08-20 09:14:36 +0000

    Add a .gitignore file

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "eu-west-1"
+    profile = "wetekdev"
+}
+
+resource "aws_instance" "wbc-preprod" {
+    ami            = "ami-01b282b0f06ba5fd2"
+    instance_type  = "t2.large"
+}
